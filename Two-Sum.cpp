4083bcac058323df795f33b6557a9b78e3d0b5dc
RenanOliveira78 <renanribeiro78@gmail.com>
2025-07-22 20:33:41 -0300

    Leetcode start

---
+++ Two-Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namesapce std;
+
+class NaiveSolutioin {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    }
+};
+
+class OptimalSolution {
+
+};
