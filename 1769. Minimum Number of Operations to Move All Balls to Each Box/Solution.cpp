234d8c8b26119fda8dee577d283f41b5bf37d3ee
RenanOliveira78 <renanribeiro78@gmail.com>
2025-07-23 11:10:21 -0300

    another nstring problem

---
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box/Solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> moves(boxes.size());
+        for(int i = 0; i < boxes.size(); i++) {
+            for(int j = 0; j < boxes.size(); j++) {
+                if(j != i && boxes[j] == '1') {
+                    // calculate the number of moves to move the stuff, if any, from box j to i
+                    // the amount of steps to move a ball sequentially is the geometric distance
+                    moves[i] +=  abs(i - j);
+                }
+            }
+        }
+        return moves;
+    }    
+};
+
+int main() {
+    string boxes = "110";
+    Solution solution;
+
+    vector<int> result = solution.minOperations(boxes);
+    
+    for(int operations : result) {
+        cout << operations;
+    }
+    
+
+    return 0;
+}
