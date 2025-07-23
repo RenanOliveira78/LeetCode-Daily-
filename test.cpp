9ac2c7417e37b956e15ddb5932023a5329c2de6a
RenanRibeiro78 <renanribeiro78@alu.ufc.br>
2025-07-23 15:37:23 -0300

    probkem solving

--- test.cpp
+++ test.cpp
@@ -2,14 +2,28 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <map>
+
 using namespace std;
 
 int main() {
-    vector<string> words = {"1", "2", "3"," wor4"};
+    string word = "bicycle";
 
-    for(string word : words) {
-        cout << word;
+    cout << word << endl;
+    
+    unordered_map <char, int> Umap;
+    map <char, int> Omap;
+
+    for(int i = 0; i < word.length(); i++) {
+        Umap[word.at(i)] = i;
     }
 
+    for(int i = 0; i < word.length(); i++) {
+        Omap[word.at(i)] = i;
+    }
+
+    cout << Umap['b'] << " " << Omap[2];
+
+    
     return 0;
 }
